d649338bd6e5137b5c1f2d12988778cadd94b9ca
TANAPORN SRIBOONPHONG <tanapon1325@gmail.com>
2020-10-12 13:49:59 +0700

    correct code

--- A_debugging_p.Quadratic/A_debugging_p.Quadratic/Quadratic Equations.cpp
+++ A_debugging_p.Quadratic/A_debugging_p.Quadratic/Quadratic Equations.cpp
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
 int main()
@@ -6,26 +7,29 @@ int main()
     double a, b, c, determinant, root1, root2, realPart, imaginaryPart;
     printf("Enter coefficients a, b and c: ");
     scanf("%lf %lf %lf", &a, &b, &c);
+    /* a = 5;
+     b = 6;
+     c = 1;*/
     determinant = b * b - 4 * a * c;
     // condition for real and different roots (#1)
     if (determinant > 0)
     {
         // sqrt() function returns square root
-        root1 = -b + sqrt(determinant) / 2 * a;
-        root2 = -b - sqrt(determinant) / 2 * a;
+        root1 = (-b + sqrt(determinant)) / (2 * a);
+        root2 = (-b - sqrt(determinant)) / (2 * a);
         printf("root1 = %.2lf and root2 = %.2lf", root1, root2); // (#2)
     }
     //condition for real and equal roots
     else if (determinant == 0)
     {
-        root1 = root2 = -b / 2 * a;
+        root1 = root2 = -b / (2 * a);
         printf("root1 = root2 = %.2lf;", root1); // (#3)
     }
     // if roots are not real 
     else
     {
-        realPart = -b / 2 * a;
-        imaginaryPart = sqrt(-determinant) / 2 * a;
+        realPart = -b / (2 * a);
+        imaginaryPart = sqrt(-determinant) / (2 * a);
         printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", realPart, imaginaryPart, realPart, imaginaryPart); // (#4)
     }
 
